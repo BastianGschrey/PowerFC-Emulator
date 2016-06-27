c1b2fb32728ede58fc82973897e3cb5d63fac0e0
Bastian Gschrey <Bastian Gschrey>
2016-06-27 21:35:12 +0200

    some fixes

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -12,7 +12,7 @@ MainWindow::MainWindow(QWidget *parent) :
     serialport = new QSerialPort(this);
     serialport->setBaudRate(QSerialPort::Baud57600);
     //serialport->setPortName("tnt1"); //used for linux with tty0tty
-    serialport->setPortName("COM10");
+    serialport->setPortName("COM5");
     serialport->setParity(serialport->NoParity);
 
     serialport->setDataBits(QSerialPort::Data8);
@@ -42,19 +42,16 @@ QByteArray dataPrefix;
 //dataPrefix[1] = 0x20;
 //dataPrefix[2] = 0x00;
 //dataPrefix[3] = 0x20;
-int index;
-int index2;
 
 //QByteArray output[33];// =  {0xF0, 0x20, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E};
 
 receivedData = serialport->readAll();
 
 
-for(index = 0; index <= 2; index++)
-{
+
 serialport->blockSignals(true);
     qDebug() <<  "Serial signal raised";
-    if(receivedData[index] == 0x0D)
+    if(receivedData[0] == 0xF0)
     {
         ui->txtConsole->append("0xF0 0x02 0x0D");
         ui->txtConsole->append("Sending reply FC info...Adv");
@@ -62,27 +59,27 @@ serialport->blockSignals(true);
         //serialport->write(QByteArray::fromHex("F0 20 02 03 04 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F 10 11 12 13 14 15 16 17 18 19 1A 1B 1C 1D 1E"));
         serialport->blockSignals(false);
 }
-    if(receivedData[index] == 0x1F)
+    if(receivedData[0] == 0xDE)
         {
             ui->txtConsole->append("0xDE 0x02 0x1F");
             ui->txtConsole->append("Sending reply FC info...Sensor Info");
-            serialport->write(QByteArray::fromHex("F0 20 00 02 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F 10 11"));
+            serialport->write(QByteArray::fromHex("DE 20 00 02 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F 10 11"));
             serialport->blockSignals(false);
     }
-    if(receivedData[index] == 0x22)
+    if(receivedData[0] == 0xDB)
         {
             ui->txtConsole->append("0xDB 0x02 0x22");
             ui->txtConsole->append("Sending reply FC info...Map Indicies");
-            serialport->write(QByteArray::fromHex("F0 20"));
+            serialport->write(QByteArray::fromHex("DB 20 20 20 20"));
             serialport->blockSignals(false);
     }
-    if(receivedData[index] == 0xFD)
+    if(receivedData[0] == 0x00)
         {
             ui->txtConsole->append("0x00 0x02 0xFD");
             ui->txtConsole->append("Sending reply FC info...Aux info");
-            serialport->write(QByteArray::fromHex("F0 20 F1 F2"));
+            serialport->write(QByteArray::fromHex("00 20 F1 F2 20"));
             serialport->blockSignals(false);
 }
 
-}
+
 }
